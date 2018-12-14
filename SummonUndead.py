1d6301b935d51b0a47e42bdd65393f042fa6bea1
Russell Neches <russell@vort.org>
2018-12-13 17:53:38 -0800

    api refinements for multiple jobs

--- SummonUndead.py
+++ SummonUndead.py
@@ -16,22 +16,19 @@ cell_template = '''
 import pickle
 from tempfile import NamedTemporaryFile
 
-{% for module in user_modules -%}
+{% for module in modules -%}
 import {{ module }}
 {% endfor %}
 
-{% for input, data in input_arguments.items() %}
-{{ input }} = pickle.loads( {{ data }} )
+{% for name, value in input.items() %}
+{{ name }} = pickle.loads( {{ value }} )
 {% endfor %}
 
 {{ cell_code }}
 
-{% for output, tmpfile in output_arguments.items() %}
-pickle.dump( {{ output }}, open( '{{ tmpfile }}', 'wb' ) )
+{% for name, tmpfile in output.items() %}
+pickle.dump( {{ name }}, open( '{{ tmpfile }}', 'wb' ) )
 {%- endfor %}
-
-with open( '/tmp/alive', 'w' ) as f :
-    f.write( "I'm alive!" )
 '''
 
 @magics_class
@@ -44,60 +41,80 @@ class SummonUndead( Magics ) :
 
     @cell_magic
     @magic_arguments()
-    @argument( '-n', '--processes', type=int, help='Number of processes to launch.' )
-    @argument( '-i', '--input',  action='append', help='Vector of input parameters.' )
+    @argument( '-l', '--label',  type=str,        help='Job label.'                   )
+    @argument( '-n', '--cpus',   type=int,        help='Number of CPUs to use.'       )
+    @argument( '-i', '--input',  type=str,        help='Vector of input parameters.'  )
     @argument( '-o', '--output', action='append', help='Vector of output parameters.' )
     def moan( self, line, cell ) :
         '''Command the zombies to moan horribly.'''
         
         args = parse_argstring( self.moan, line )
-       
-        # get the user input variables, pickle them, and stash them in a dictionary
-        input_arguments = {}
+        
+        # input should be a list of dictionaries
+        input_vector = [] 
         if args.input :
-            for input in ','.join( args.input ).split( ',' ) :
-                try :
-                    val = self.shell.user_ns[ input ]
-                    input_arguments[ input ] = pickle.dumps( val )
-                except KeyError :
-                    raise NameError( "name '%s' is not defined" % input )
+            try :
+                input_vector = self.shell.user_ns[ args.input ]
+                
+                if not isinstance( input_vector, list ) :
+                    raise NameError( 'Input vector must be a list of dictionaries.' )
+                
+                if len( input_vector ) == 0 :
+                    raise NameError( 'Input cannot be empty.' )
+                
+                if not isinstance( input_vector[0], dict ) :
+                    raise NameError( 'Input vector must be a list of dictionaries.' )
+                
+                for run in input_vector :
+                    for key, value in run.items() :
+                        run[key] = pickle.dumps( value )
+                
+            except KeyError :
+                raise NameError( "name '%s' is not defined" % args.input )
         
         # get the user output variable names, create temp files for each
-        output_arguments = {}
+        output = {}
         if args.output :
-            for output in ','.join( args.output ).split( ',' ) :
-                output_arguments[ output ] = NamedTemporaryFile( mode='w', suffix='.output', delete=False ).name
+            for var in ','.join( args.output ).split( ',' ) :
+                output[ var ] = NamedTemporaryFile( mode='w', suffix='.output', delete=False ).name
         
         # figure out what modules the user has loaded
-        user_modules = set( key for key,value in self.shell.user_ns.items() if isinstance( value, types.ModuleType ) )
-        user_modules = user_modules - set( [ '__builtin__', '__builtins__' ] )
+        modules = set( key for key,value in self.shell.user_ns.items() if isinstance( value, types.ModuleType ) )
+        modules = modules - set( [ '__builtin__', '__builtins__' ] )
         
-        # generate the cell-script from the template
-        with NamedTemporaryFile( mode='w', suffix='.cell', delete=False ) as f :
-            f.write( self.cell_template.render( user_modules     = user_modules,
-                                                input_arguments  = input_arguments,
-                                                output_arguments = output_arguments, 
-                                                cell_code        = cell ) )
-            
-            
-            cell_script = f.name
-        
-        # execute the cell
-        p = subprocess.Popen( [ 'python', str(cell_script) ],
-                              stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE )
+        output_vector = []
+        for input in input_vector :
+            self._execute( cell, input, output, modules )
+            run_output = {}
+            for key, tmpfile in output.items() :
+                run_output[ key ] = pickle.load( open( tmpfile, 'rb' ) )
+            output_vector.append( run_output )
 
-        p.communicate()
-        
-        # put recovered output variables into the user's namespace
-        for output, tmpfile in output_arguments.items() :
-            self.shell.push( { output : pickle.load( open( tmpfile, 'rb' ) ) } )
-            #self.shell.push( { output : open( tmpfile ) } )
+        self.shell.push( { args.label + '_output' : output_vector } )
 
     @line_magic
     def summon_undead( self, line ) :
         
         return 'Army of undead summoned. ' 
+
+    def _execute( self, cell_code, input, output, modules ) :
+        '''Execute the cell code.'''
+
+        # generate the cell-script from the template
+        with NamedTemporaryFile( mode='w', suffix='.cell', delete=False ) as f :
+            f.write( self.cell_template.render( modules   = modules,
+                                                input     = input,
+                                                output    = output, 
+                                                cell_code = cell_code ) )
+            
+            cell_script = f.name
+ 
+        # execute the cell
+        p = subprocess.Popen( [ 'python', str(cell_script) ],
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE )
+        stdout, stderr = p.communicate()
+
 
 def load_ipython_extension( ip ) :
     '''Load extension in IPython.'''
